23094019fc98585dcb0d7afbf12c93eba5ce195a
aryan55254 <aryanmishra55254@gmail.com>
2025-01-15 19:45:18 +0530

    shadcn setup

--- src/components/Hero2.jsx
+++ src/components/Hero2.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Hero2() {
   return (
     <>
-    <div className='bg-black mt-10 p-3 lg:mt-20 lg:p-10'>
+    <div className='bg-black  p-3 lg:mt-20 lg:p-10'>
         <h1 className='font-bebas font-bold text-slate-400 text-center text-6xl  '>Your Ultimate Movie Hub</h1>
     </div>
     <div className='bg-black py-5 lg:py-10'>
--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function slider() {
   return (
-    <div>slider</div>
+    <>
+    <div className='bg-blue-700 bg-opacity-10  my-5'>
+    <h1 className='bg-gradient-to-t from-blue-600 via-slate-600 to-pink-500 text-center text-transparent font-bebas  text-glow font-bold mx-15 my-5 text-6xl bg-clip-text p-5'>TRENDING MOVIES THIS WEEK</h1>
+
+
+    </div>
+    
+  
+    </>
   )
 }
 
